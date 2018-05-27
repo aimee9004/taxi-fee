dfb085084b25b15b59f4ee3842990e441375d624
aimee <826065426@qq.com>
2018-05-28 01:35:49 +0800

    完成

--- main/main.js
+++ main/main.js
@@ -1,4 +1,19 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(distance, holdTime) {
+    if(distance <= 2) {
+        return getRound(6 + getHoldTimePrice(holdTime))
+    }else if(distance > 2 && distance <= 8) {
+        return getRound(6 + (distance - 2) * 0.8 + getHoldTimePrice(holdTime))
+    }else if(distance > 8) {
+        return getRound(6 + 4.8 + (distance - 8) * 1.2 + getHoldTimePrice(holdTime))
+    }
 };
+function getRound(num) {
+    return Math.round(num)
+}
+function getHoldTimePrice(holdTime) {
+    if(holdTime > 0) {
+        return holdTime * 0.25
+    }else {
+        return 0
+    }
+}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,4 +2,35 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    it('2公里内起步价：', function() {
+        var result = main(2);
+        expect(6).toEqual(result);
+    })
+
+    it('2公里内起步价,停车等待加收是：', function() {
+        var result = main(2, 2);
+        expect(7).toEqual(result);
+    })
+
+    it('2公里外，8公里内：', function() {
+        var result = main(3);
+        expect(7).toEqual(result);
+    })
+
+    it('2公里外，8公里内，停车等待：', function() {
+        var result = main(4, 2);
+        expect(8).toEqual(result);
+    })
+
+    it('8公里外', function() {
+        var result = main(10);
+        expect(13).toEqual(result);
+    })
+
+    it('8公里外,停车等待', function() {
+        var result = main(10, 5);
+        expect(14).toEqual(result);
+    })
+
 });
+
